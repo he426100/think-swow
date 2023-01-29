e5b60f030dc2926981c45a0276d94624873d252b
he426100 <9689137+he426100@users.noreply.github.com>
2023-01-30 02:36:55 +0800

    用msleep代替channel

--- src/coroutine/crontab/Crontab.php
+++ src/coroutine/crontab/Crontab.php
@@ -3,7 +3,7 @@
 namespace think\swow\coroutine\crontab;
 
 use think\swow\Coroutine;
-use think\swow\Channel;
+use function msleep;
 
 class Crontab
 {
@@ -21,21 +21,19 @@ class Crontab
             $parser = new Parser();
             while (1) {
                 $current = (int)(microdate('s.v') * 1000);
-                $sleep = (60_000 - $current) / 1000;
-                $channel = new Channel();
-                $channel->pop($sleep ?: 0.001);
+                $sleep = 60_000 - $current;
+                msleep($sleep ?: 1);
  
                 foreach ($this->crontabs as $crontab) {
                     list($rule, $func) = $crontab;
                     $times = $parser->parse($rule);
                     foreach ($times as $time) {
                         Coroutine::create(static function () use ($time, $func) {
-                            $wait = $time - time();
+                            $wait = ($time - time()) * 1000;
                             if ($wait <= 0) {
-                                $wait = 0.001;
+                                $wait = 1;
                             }
-                            $channel = new Channel();
-                            $channel->pop((float)$wait);
+                            msleep($wait);
                             $func();
                         });
                     }
